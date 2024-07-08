b86cccb850d3d6ae8119199a4c5dc278828e14d7
praful-36 <kyngaming36@gmail.com>
2024-07-08 19:01:13 +0530

    Error Fixing changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Telegram-clone-project/'  // Replace 'your-repo-name' with your actual repo name
+});
